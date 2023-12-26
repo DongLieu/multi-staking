bfc4ae6ce47780f8eb9d47db30d5f0a56103cda0
DongLieu <duongduydongtq@gmail.com>
2023-12-26 14:37:11 +0700

    TestAccAddrAndValAddrFromStrings

--- x/multi-staking/types/utils.go
+++ x/multi-staking/types/utils.go
@@ -4,12 +4,12 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-func AccAddrAndValAddrFromStrings(accAddrString string, valAddrStraing string) (sdk.AccAddress, sdk.ValAddress, error) {
+func AccAddrAndValAddrFromStrings(accAddrString string, valAddrString string) (sdk.AccAddress, sdk.ValAddress, error) {
 	accAddr, err := sdk.AccAddressFromBech32(accAddrString)
 	if err != nil {
 		return sdk.AccAddress{}, sdk.ValAddress{}, err
 	}
-	valAcc, err := sdk.ValAddressFromBech32(valAddrStraing)
+	valAcc, err := sdk.ValAddressFromBech32(valAddrString)
 	if err != nil {
 		return sdk.AccAddress{}, sdk.ValAddress{}, err
 	}
